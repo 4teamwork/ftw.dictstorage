ce17213c5afb5c13df6106d4871498d494684759
Philippe Gross <p.gross@4teamwork.ch>
2010-11-16 08:53:11 +0100

    updated setup.py (4teamwork standards)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-
-version = '0.1'
+version = open('src/ftw/dictstorage/version.txt').read().strip()
+maintainer = 'Rok Garbas'
 
 tests_require = [
             'unittest2',
@@ -18,7 +18,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
     keywords='fwt 4teamwork key value storage sqlalchemy',
-    author='Rok Garbas',
+    author='%s, 4teamwork GmbH' % maintainer,
     author_email='rok@garbas.si',
     url='gitolite@git.4teamwork.ch:ftw/ftw.dictstorage.git',
     license='GPL',
