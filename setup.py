15289f6fb30c8d9109b94aa1ef4e884aa7327b9b
Jonas Baumann <jone@jone.ch>
2013-01-28 11:07:57 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -15,7 +15,14 @@ setup(
     long_description=open('README.rst').read() + '\n' + \
         open('docs/HISTORY.txt').read(),
 
-    classifiers=[
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
